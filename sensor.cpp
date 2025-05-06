c5d01ac4616d029ccebe64d6d9f234509c2443b7
Ocean <156903851+Oceandek@users.noreply.github.com>
2025-05-06 16:37:37 +0200

    Add files via upload

--- sensor.cpp
+++ sensor.cpp
@@ -1,1 +1,19 @@
+#include "sensor.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Temperatuursensor::Temperatuursensor() {
+    temperatuur = 12;
+}
+
+void Temperatuursensor::settemperatuursensor(float i) {
+    temperatuur = i;
+}
+
+
+float  Temperatuursensor::gettemperatuursensor() {
+    return temperatuur ;
+}
+
 
--- sensor.h
+++ sensor.h
@@ -1,1 +1,20 @@
+#ifndef SENSOR_H
+#define SENSOR_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Temperatuursensor {
+    private:
+    int temperatuur;
+
+    public:
+    Temperatuursensor();
+    void settemperatuursensor(float t);
+    float gettemperatuursensor();
+
+
+};
+
+#endif
