67981b7a09e3906873355a6bf9dd64f32b5bf409
Ocean <156903851+Oceandek@users.noreply.github.com>
2025-05-13 15:23:58 +0200

    Update AircoControle.cpp

--- AircoControle.cpp
+++ AircoControle.cpp
@@ -1,19 +1,1 @@
-#include "sensor.h"
-#include <iostream>
-#include <string>
-using namespace std;
-
-Temperatuursensor::Temperatuursensor() {
-    temperatuur = 12;
-}
-
-void Temperatuursensor::settemperatuursensor(float i) {
-    temperatuur = i;
-}
-
-
-float  Temperatuursensor::gettemperatuursensor() {
-    return temperatuur ;
-}
-
 
