65e89c70f51097ad817747736c6c459ee9bd112a
MauroVM8 <156448335+MauroVM8@users.noreply.github.com>
2025-05-20 16:37:34 +0200

    Update main.cpp EERSTE COMMIT VOOR main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
 
+#ifndef SENSOR_H
+#define SENSOR_H
+#ifndef ACTURATOR_H
+#define ACTURATOR_H
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int Drukknop::getdrukknopstatus() {
+    return value;
+    }
+    void Temperatuursensor::settemperatuursensor(float i) {
+    temperatuur = i;
+    if i >= 25.0
+
+
+    if i >= 25.0 AND value != 999 {
+        void LEDs::groenAan() {
+        }
+
+    else 
+
+
+    return 0;
+}
+
+
+
+
+
+
+#endif
+#endif
