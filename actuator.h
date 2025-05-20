b59615c403f4cc7a1194d6c9200556e02472e152
vitmin415 <156904697+vitmin415@users.noreply.github.com>
2025-05-20 14:09:34 +0200

    Update actuator.h
    Hierbij heb ik klasses aangemaakt (voor de actuator, voor de Buzzer, voor de LEDs ook)

--- actuator.h
+++ actuator.h
@@ -1,1 +1,40 @@
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+#include <Arduino.h>
+
+class AircoActuator {
+  private:
+    int motorPin;
+
+  public:
+    AircoActuator(int motorpin);
+    void begin();
+    void Aan(bool aan);
+};
+
+class Buzzer {
+  private:
+    int pin;
+
+  public:
+    Buzzer(int pin);
+    void begin();
+    void Aan(bool aan);
+};
+
+class LEDS {
+  private:
+    int groenPin;
+    int roodPin;
+
+  public:
+    StatusLEDs(int groenPin, int roodPin);
+    void begin();
+    void groenAan();
+    void roodAan();
+    void allesUit();
+};
+
+#endif
 
