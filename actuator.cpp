5fb78e437afd37b44238a355684f00b59200b05d
vitmin415 <156904697+vitmin415@users.noreply.github.com>
2025-05-20 15:33:19 +0200

    Update actuator.cpp
    secundaire ccp voor leds en actuator aangemaakt, desnoods altijd aanpassen

--- actuator.cpp
+++ actuator.cpp
@@ -1,1 +1,57 @@
+#include "Actuator.h"
+#include <Arduino.h>
+
+Actuator::Actuator(int pin) {
+    motorPin = pin;
+}
+
+void Actuator::begin() {
+    pinMode(motorPin, OUTPUT);
+    digitalWrite(motorPin, LOW);
+}
+
+void Actuator::aanzetten(bool aan) {
+    digitalWrite(motorPin, HIGH);
+}
+
+
+LEDs::LEDs(int g, int r, int b) {
+    groenPin = g;
+    roodPin = r;
+    blauwPin = b;
+}
+
+void LEDs::begin() {
+    pinMode(groenPin, OUTPUT);
+    pinMode(roodPin, OUTPUT);
+    pinMode(blauwPin, OUTPUT);
+    allesUit();
+}
+
+void LEDs::groenAan() {
+    digitalWrite(groenPin, HIGH);
+    digitalWrite(roodPin, LOW);
+    digitalWrite(blauwPin, LOW);
+}
+
+void LEDs::roodAan() {
+    digitalWrite(groenPin, LOW);
+    digitalWrite(roodPin, HIGH);
+    digitalWrite(blauwPin, LOW);
+}
+
+void LEDs::blauwKnipperen() {
+    for (int i = 0; i < 5; i++) {
+        digitalWrite(blauwPin, HIGH);
+        delay(200);
+        digitalWrite(blauwPin, LOW);
+        delay(200);
+    }
+}
+
+void LEDs::allesUit() {
+    digitalWrite(groenPin, LOW);
+    digitalWrite(roodPin, LOW);
+    digitalWrite(blauwPin, LOW);
+}
 
