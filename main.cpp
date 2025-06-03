efb6c9d5c6979094e6ec93d0347102ee2a68d697
Ocean <156903851+Oceandek@users.noreply.github.com>
2025-06-03 16:34:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,21 @@
-#include <Arduino.h>
+#include <Arduino.h>More actions
 #include "JSONAPI.h"
 #include "actuator.h"
 #include "sensor.h"
 
-
-//--
-
 // JsonbinSender instellen met je gegevens
 JsonbinSender sender("VTI-Guest", "Wifi0104", "6835ae4e8561e97a501c1c7d", "$2a$10$w30Ri0Qr84FdWX3uwwquC..Cp0OVIlTiHCSedLYFv6aYUkF2GQS2e");
 
-//--
-
 // Sensor- en actuatorobjecten
 Temperatuursensor tempSensor;
-Drukknop knop(15); // Pas pin aan voor de drukknop
-LEDS leds(2, 3, 4); // Pas aan naar je echte LED-pinnen
-Actuator airco(5);  // Pas aan naar je motorPin
-
-//--
+Drukknop knop(15); 
+LEDs leds(2, 4, 5); 
+Actuator airco(18);  
 
 // LED-statusen bijhouden
 bool groen = false;
 bool blauw = false;
 bool rood = false;
-
-//--
 
 void setup() {
     Serial.begin(115200);
@@ -35,47 +26,47 @@ void setup() {
     airco.begin();
 }
 
-//--
-
 void loop() {
     float temperatuur = tempSensor.gettemperatuursensor();
     bool knopstatus = knop.getdrukknopstatus();
 
-    // Reset LED-statusen
     groen = false;
     blauw = false;
     rood = false;
 
-    // Bepaal logica
     bool aircoAan = false;
 
-    if (temperatuur >= 25.0 && knopstatus) {
+    if (temperatuur >= 20.0 && knopstatus) {
         Serial.println("De airco is actief.");
-        leds.groenAan();
+        leds.groenaan();
         groen = true;
         airco.aanzetten(true);
         aircoAan = true;
 
-    } else if (temperatuur >= 25.0 && !knopstatus) {
+    } else if (temperatuur >= 20.0 && !knopstatus) {
         Serial.println("De airco stuurt een waarschuwing.");
         leds.blauwKnipperen();
-        blauw = true;  // Let op: je knippert de LED, we zetten 'blauw' nu tijdelijk aan
+        leds.roodaan();
+
+        blauw = true;  
+        rood = true;
         airco.aanzetten(false);
+        aircoAan = false;
 
     } else {
         Serial.println("De airco is uit.");
-        leds.roodAan();
+
+        leds.roodaan();
         rood = true;
         airco.aanzetten(false);
+        aircoAan = false;
+
     }
 
-    // Simuleer raamstatus (drukknop = raam open/dicht)
     bool raamOpen = !knopstatus;
 
     // Stuur data naar Jsonbin
     sender.sendData(temperatuur, knopstatus, groen, blauw, rood, raamOpen, aircoAan);
 
-    delay(60000); // Wacht 1 minuut
+    delay(20000); // Wacht 20 seconden
 }
-
-//--
