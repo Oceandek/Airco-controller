0d280394fba81da029c05a38fdd4ccb037c63c63
Ocean <oceandekeyser2@gmail.com>
2025-06-03 14:14:53 +0200

    syntax errors + functies hernoemd

---
+++ sketch/JSONAPI.h
@@ -0,0 +1,69 @@
+#ifndef JSONAPI_H
+#define JSONAPI_H
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+class JsonbinSender {
+private:
+    const char* ssid;
+    const char* password;
+    const char* binID;
+    const char* apiKey;
+    String url;
+
+public:
+    JsonbinSender(const char* wifiSSID, const char* wifiPassword, const char* bin, const char* key)
+        : ssid(wifiSSID), password(wifiPassword), binID(bin), apiKey(key), url(String("https://api.jsonbin.io/v3/b/") + bin) {}
+
+    void connectWiFi() {
+        WiFi.begin(ssid, password);
+        Serial.print("Verbinden met WiFi...");
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+        }
+        Serial.println("verbonden!");
+    }
+
+    void sendData(float temperatuur, bool knopwaarde, bool groen, bool blauw, bool rood, bool raam, bool airco) {
+        if (WiFi.status() == WL_CONNECTED) {
+            HTTPClient http;
+
+            // JSON payload opbouwen
+            StaticJsonDocument<200> doc;
+            doc["temperatuur"] = temperatuur;
+            doc["knopwaarde"] = knopwaarde;
+            JsonObject leds = doc.createNestedObject("leds");
+            leds["groen"] = groen;
+            leds["blauw"] = blauw;
+            leds["rood"] = rood;
+            doc["raam"] = raam;
+            doc["airco"] = airco;
+
+            String jsonStr;
+            serializeJson(doc, jsonStr);
+
+            http.begin(url);
+            http.addHeader("Content-Type", "application/json");
+            http.addHeader("X-Master-Key", apiKey);
+
+            int httpCode = http.PUT(jsonStr);
+
+            if (httpCode > 0) {
+                Serial.printf("Response code: %d\n", httpCode);
+                String payload = http.getString();
+                Serial.println(payload);
+            } else {
+                Serial.printf("Error bij versturen: %s\n", http.errorToString(httpCode).c_str());
+            }
+
+            http.end();
+        } else {
+            Serial.println("WiFi niet verbonden!");
+        }
+    }
+};
+
+#endif
---
+++ sketch/actuator.h
@@ -0,0 +1,32 @@
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+#include <Arduino.h>
+
+class Actuator {
+  private:
+    int motorPin;
+
+  public:
+    Actuator(int motorpin);
+    void begin();
+    void aanzetten(bool aan);
+};
+
+class LEDs {
+  private:
+    int groenPin;
+    int roodPin;
+    int blauwPin;
+
+  public:
+    LEDs(int groenPin, int roodPin, int blauwPin);
+    void begin();
+    void groenaan();
+    void roodaan();
+    void blauwPin();
+    void allesuit();
+};
+
+#endif
+
